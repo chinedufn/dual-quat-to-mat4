477d088f23594fed6b7cafd932a6258f910a042f
Chinedu Francis Nwafili <frankie.nwafili@gmail.com>
2017-02-08 20:18:17 -0500

    Fix grammar

--- src/dual-quat-to-mat4.js
+++ src/dual-quat-to-mat4.js
@@ -4,7 +4,7 @@ module.exports = convertDualQuatToMatrix
 //  recommended reading: https://www.cs.utah.edu/~ladislav/kavan07skinning/kavan07skinning.pdf
 function convertDualQuatToMatrix (dq) {
   // dq[0] dq[1] dq[2] dq[3] are our rotation quaternion elements
-  // dq[4] dq[5] dq[6] dq[7] are our translation quaternion elements (the dual quaternions imaginary component)
+  // dq[4] dq[5] dq[6] dq[7] are our translation quaternion elements (the dual quaternion's imaginary component)
   return [
     1.0 - (2.0 * dq[1] * dq[1]) - (2.0 * dq[2] * dq[2]),
     (2.0 * dq[0] * dq[1]) + (2.0 * dq[3] * dq[2]),
